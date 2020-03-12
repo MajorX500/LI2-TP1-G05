a24c57384d95c45572a1ae3a1f91def4731feab6
pr0t0typ3-ZeR0 <guilherme2001fernandes@hotmail.com>
2020-03-12 11:31:37 +0000

    Completed data layer (G5)

--- data.c
+++ data.c
@@ -1,27 +1,4 @@
-typedef enum {EMPTY, WHITE, BLACK} HOUSE;
-
-typedef struct 
-{
-    int x; //columns
-    int y; //lines
-} COORDINATES;
-
-typedef struct 
-{
-    COORDINATES player1;
-    COORDINATES player2;
-} MOVE;
-
-typedef MOVE MOVES[32];
-
-typedef struct 
-{
-    HOUSE board[8][8]; //board state
-    COORDINATES previous_move;
-    MOVES moves;
-    int num_moves;
-    int current_player;
-} STATE;
+#include "data.h"
 
 void empty_board(HOUSE board[8][8])
 {
@@ -53,6 +30,10 @@ int get_current_player(STATE *s)
 
 HOUSE get_house(STATE *s, COORDINATES c)
 {
-    HOUSE b[8][8] = s -> board;
-    return b[c.y][c.x];
+    return s -> board[c.y][c.x];
+}
+
+int get_num_moves(STATE *s)
+{
+    return s -> num_moves;
 }
---
+++ data.h
@@ -0,0 +1,29 @@
+#ifndef __DATA_H__
+#define __DATA_H__
+
+typedef enum {EMPTY, WHITE, BLACK} HOUSE;
+
+typedef struct 
+{
+    int x; //columns
+    int y; //lines
+} COORDINATES;
+
+typedef struct 
+{
+    COORDINATES player1;
+    COORDINATES player2;
+} MOVE;
+
+typedef MOVE MOVES[32];
+
+typedef struct 
+{
+    HOUSE board[8][8]; //board state
+    COORDINATES previous_move;
+    MOVES moves;
+    int num_moves;
+    int current_player;
+} STATE;
+
+#endif
